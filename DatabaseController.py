ab384037b92f770bc493cc80dccbc9a7c5e7bef5
Paolo <paoloalberto95@gmail.com>
2020-04-20 22:18:52 +0200

    multiple cards and other stuff

--- DatabaseController.py
+++ DatabaseController.py
@@ -12,18 +12,18 @@ class Room:
     users_collection = mydb["users"]
 
     def __init__(self, room_name):
-    	self.room_document = {"room_name": room_name, "used_black_cards": [], "users_list": []}
-    	insert_room = self.room_collection.insert_one(self.room_document)
-    	self.room_id = insert_room.inserted_id
+        room_document = {"room_name": room_name, "used_black_cards": [], "users_list": []}
+        insert_room = self.room_collection.insert_one(room_document)
+        self.room_id = insert_room.inserted_id
 
     def add_new_user(self, user_name):
-    	user_doc = {"name": user_name, "points": 0, "cards_in_hand": [], "used_white_cards": []}
-    	insert_user = self.users_collection.insert_one(user_doc)
-    	self.room_collection.update_one({"_id" : self.room_id}, { "$push": { "users_list": insert_user.inserted_id} })
-    	return insert_user.inserted_id
+        user_doc = {"name": user_name, "points": 0, "cards_in_hand": [], "used_white_cards": []}
+        insert_user = self.users_collection.insert_one(user_doc)
+        self.room_collection.update_one({"_id" : self.room_id}, { "$push": { "users_list": insert_user.inserted_id} })
+        return insert_user.inserted_id
 
     def user_wins(self, winning_user_id):
-    	self.users_collection.update_one({"_id" : winning_user_id}, { "$inc": { "points": 1 } })
+        self.users_collection.update_one({"_id" : winning_user_id}, { "$inc": { "points": 1 } })
 
 
     def add_new_cards(self, input_card_list):  # Add new documents from a list
@@ -47,18 +47,19 @@ class Room:
         self.cards_collection.insert_one(doc)
 
     def pick_random_black_card(self):  # Returns text and pick value of a random black card
-    	myquery = { "pick": { "$gt": 0 }, "_id": { "$nin": self.room_document["used_black_cards"]} }  # Query to select unused black cards
-    	myresults = self.cards_collection.find(myquery)
-    	result_card = myresults[random.randint(0, myresults.count()-1)] # Select random document
-    	# Add card to already used list:
-    	self.room_collection.update_one({"_id" : self.room_id}, { "$push": { "used_black_cards": result_card["_id"]} })
-    	return result_card["text"], result_card["pick"]
+        list_of_used_cards = self.room_collection.find_one({"_id": self.room_id})["used_black_cards"]  # Find used black cards
+        myquery = { "pick": { "$gt": 0 }, "_id": { "$nin": list_of_used_cards} }  # Query to select all unused black cards
+        myresults = self.cards_collection.find(myquery)
+        result_card = myresults[random.randint(0, myresults.count()-1)] # Select random document
+        # Add card to already used list:
+        self.room_collection.update_one({"_id" : self.room_id}, { "$push": { "used_black_cards": result_card["_id"]} })
+        return result_card["text"], result_card["pick"]
 
-    def pick_random_white_card(self, user_id):  # Takes the user who is picking the card as an argument
-    	myquery = { "pick": 0 }
-    	myresults = self.cards_collection.find(myquery)
-    	result_card = myresults[random.randint(0, myresults.count()-1)] # Select random document
-    	# Updates the list of cards already used by this user:
-    	self.users_collection.update_one({"_id" : user_id}, { "$push": { "used_white_cards": result_card["_id"]} })
-    	self.users_collection.update_one({"_id" : user_id}, { "$push": { "cards_in_hand": result_card["_id"]} })
-    	return result_card["text"]	
+    def pick_n_random_white_cards(self, n, user_id):  # Takes the user who is picking the card as an argument
+        myquery = { "pick": 0 }
+        myresults = list(self.cards_collection.find(myquery)) 
+        result_cards = random.sample(myresults, n)
+        for rc in result_cards:  # Updates the list of cards already used by this person:
+            self.users_collection.update_one({"_id" : user_id}, { "$push": { "used_white_cards": rc["_id"]} })
+            self.users_collection.update_one({"_id" : user_id}, { "$push": { "cards_in_hand": rc["_id"]} })
+        return [rc["text"]  for rc in result_cards]

a3c390e21e58f9635f6489fdec55e16334a4fe2e
0000matteo0000 <matteo.sid@hotmail.it>
2020-04-23 23:58:56 +0200

    index

--- flask/website.py
+++ flask/website.py
@@ -5,4 +5,31 @@ website = Blueprint('website', __name__,)
 
 @website.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('index.html',
+    rooms=[
+        {"name": "room1"},
+        {"name": "room2"},
+        {"name": "room3"},
+        {"name": "room4"},
+        {"name": "room5"},
+        {"name": "room6"},
+    ],
+    player_rooms=[
+        {
+            "name": "room1",
+            "players": [
+                {"name": "player1"},
+                {"name": "player2"},
+                {"name": "player3"},
+            ],
+        },
+        {
+            "name": "room2",
+            "players": [
+                {"name": "player4"},
+                {"name": "player5"},
+                {"name": "player6"},
+            ],
+        },
+    ]
+    )

3099447f444015eb8ea7312d40a08f6e9ce46528
0000matteo0000 <matteo.sid@hotmail.it>
2020-04-23 23:12:27 +0200

    folders?

--- runme.sh
+++ runme.sh
@@ -5,6 +5,5 @@ cd "$here"
 lsof "./venv/bin/python" | awk 'NR > 1 {print $2}' | xargs kill || :
 source "./venv/Scripts/activate" || source "./venv/bin/activate"
 # start flask server
-ls
 cd "$here/flask"
 FLASK_ENV=development FLASK_APP="server.py" python -m flask run 2>&1 | tee -a "./log.txt"

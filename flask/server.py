7c221aa18733a69a70b17a1e5116e7721afb1367
0000matteo0000 <matteo.sid@hotmail.it>
2020-04-24 00:42:04 +0200

    base template and example

--- flask/server.py
+++ flask/server.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 
 app.register_blueprint(api)
 app.register_blueprint(website)
+
+@app.route('/biscottini', methods = ['POST', 'GET'])
+def setcookie():
+   resp = make_response(render_template('readcookie.html'))
+   resp.set_cookie('userID', user)
+   return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome ' + name + '</h1>'

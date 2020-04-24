00b2b4079b089cb3a71b1bf66c85d385299d030e
0000matteo0000 <matteo.sid@hotmail.it>
2020-04-24 02:26:51 +0200

    fontawesome

--- flask/server.py
+++ flask/server.py
@@ -1,20 +1,27 @@
 from api import *
-from flask import Flask
+from flask import Flask, url_for
 from website import *
 
 app = Flask(__name__)
 
+
 app.register_blueprint(api)
 app.register_blueprint(website)
 
-@app.route('/biscottini', methods = ['POST', 'GET'])
+
+@app.route('/favicon.ico')
+def favicon():
+    return url_for('static', filename='favicon.ico')
+
+
+@app.route('/biscottini', methods=['POST', 'GET'])
 def setcookie():
-   resp = make_response(render_template('readcookie.html'))
-   resp.set_cookie('userID', user)
-   return resp
+    resp = make_response(render_template('readcookie.html'))
+    resp.set_cookie('userID', user)
+    return resp
 
 
 @app.route('/getcookie')
 def getcookie():
-   name = request.cookies.get('userID')
-   return '<h1>welcome ' + name + '</h1>'
+    name = request.cookies.get('userID')
+    return '<h1>welcome ' + name + '</h1>'
